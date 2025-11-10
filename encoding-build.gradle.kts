cec58a2d0f256de6b21a7d9a0575d955396a15de
Pussaley <98553764+Pussaley@users.noreply.github.com>
2025-11-10 16:55:36 +0300

    Create encoding-build.gradle.kts

---
+++ encoding-build.gradle.kts
@@ -0,0 +1,11 @@
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs(
+        "-Dfile.encoding=UTF-8",
+        "-Dsun.stdout.encoding=UTF-8",
+        "-Dsun.stderr.encoding=UTF-8"
+    )
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
